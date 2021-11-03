9395aebfae8dbe7a3b7823b53abf69bd1e40d8b5
Rob Cameron <rob.cameron@fastmail.com>
2021-11-02 17:53:57 -0700

    Remove comments

--- api/src/services/posts/posts.js
+++ api/src/services/posts/posts.js
@@ -3,19 +3,19 @@ export const posts = async () => {
 }
 
 export const post = ({ id }) => {
-  return context.currentUser.posts().find(id)
+  return context.currentUser.posts.find(id)
 }
 
 export const createPost = ({ input }) => {
-  return context.currentUser.posts().create(input)
+  return context.currentUser.posts.create(input)
 }
 
 export const updatePost = async ({ id, input }) => {
-  const post = await context.currentUser.posts().find(id)
+  const post = await context.currentUser.posts.find(id)
   return post.update(input)
 }
 
 export const deletePost = async ({ id }) => {
-  const post = await context.currentUser.posts().find(id)
+  const post = await context.currentUser.posts.find(id)
   return post.destroy()
 }
