7606b77f934fbf80dc8fab5cf1e447e20a2ecd8c
Rob Cameron <rob.cameron@fastmail.com>
2021-11-05 17:15:40 -0700

    Moves requiredModel defintions to index

--- api/src/models/index.js
+++ api/src/models/index.js
@@ -1,1 +1,7 @@
-export * from './models'
+import User from './User'
+import Post from './Post'
+
+Post.requiredModels = [User]
+User.requiredModels = [Post]
+
+export { User, Post }
--- api/src/models/models.js
+++
@@ -1,7 +0,0 @@
-import User from './User'
-import Post from './Post'
-
-Post.requiredModels = [User]
-User.requiredModels = [Post]
-
-export { User, Post }
